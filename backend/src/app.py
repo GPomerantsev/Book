60909af284cc33eb7335a41f1f71fb43d03a61c6
GPomerantsev <gleb.pomerantsev@gmail.com>
2020-10-20 19:34:03 +0300

    add common comands

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 
@@ -7,6 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World</h1>'
+
+@app.route('/notes/', methods = ['POST'])
+def createNote():
+    print(request.json)
+    return 'received'
+
+@app.route('/notes/', methods = ['GET'])
+def getNotes():
+    return 'received'
+
+@app.route('/notes/<id>', methods = ['PUT'])
+def updateNote():
+    return 'received'
+
+@app.route('/notes/<id>', methods = ['GET'])
+def deleteNote():
+    return 'received'
 
 if __name__ == "__main__":
     app.run(debug=True)
